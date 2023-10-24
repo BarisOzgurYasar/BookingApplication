41d5b78013712624e6209e4cf506f9713449602e
Barış Özgür Yaşar <yasarbarisozgur@gmail.com>
2023-10-24 13:19:16 +0300

    BookingApplication

---
+++ api/routes/hotels.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import Hotel from '../models/Hotel.js';
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from '../controller/hotelController.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
+const router = express.Router();
+
+//CREATE
+router.post('/', verifyAdmin, createHotel);
+
+//UPDATE
+router.put('/:id', verifyAdmin, updateHotel);
+
+//DELETE
+router.delete('/:id', verifyAdmin, deleteHotel);
+
+//GET
+router.get('/:id', getHotel);
+
+//GET ALL
+router.get('/', getHotels);
+
+export default router;
