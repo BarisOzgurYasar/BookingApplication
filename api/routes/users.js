1b75ebf4aeb75a2d93bbd19afa07d123d353d810
Barış Özgür Yaşar <yasarbarisozgur@gmail.com>
2023-10-25 13:56:21 +0300

    BookingApp

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,7 +9,7 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-/* router.get('/checkauthentication', verifyToken, (req, res, next) => {
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
   res.send('hello user, you are logged in');
 });
 router.get('/checkuser/:id', verifyUser, (req, res, next) => {
@@ -17,7 +17,7 @@ router.get('/checkuser/:id', verifyUser, (req, res, next) => {
 });
 router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
   res.send('hello admin, you are logged in and you can delete all accounts');
-}); */
+});
 
 //UPDATE
 router.put('/:id', verifyUser, updateUser);
